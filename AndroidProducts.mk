6adb287a97eba48807ec4b90ed1823bcb667095b
claudfisch <92691582+claudfisch@users.noreply.github.com>
2025-07-21 15:31:24 +0200

    An empty one and a relink to another file mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# SPDX-FileCopyrightText: 2022 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Old entry
+#PRODUCT_MAKEFILES := $(LOCAL_DIR)/rhodec.mk
+
+# New entry
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_rhode.mk
